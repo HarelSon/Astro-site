45f38968aff03e066cbf5131bbd803f8d12a7a8d
Henry Ramirez <harelson3@gmail.com>
2023-03-23 10:23:43 -0500

    Add configuration container theme.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: "1rem",
+
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1024px",
+        //"2xl": "1496px",
+      },
+    },
     extend: {
       colors: {
         fis: {
